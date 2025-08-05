4e2eb733e3b6e838f62978c33ee493d3a35f3e43
ALbert-Shen-code <71628709+ALbert-Shen-code@users.noreply.github.com>
2025-08-05 13:21:00 +1000

    update

--- src/dynamodb-container.ts
+++ src/dynamodb-container.ts
@@ -123,8 +123,10 @@ export class StartedDynamoDBContainer implements StartedTestContainer {
     protocol?: string
   ): number {
     if (typeof port === 'number') {
+      // @ts-expect-error - testcontainers typing does not support two args
       return this.startedContainer.getMappedPort(port, protocol);
     }
+    // @ts-expect-error - testcontainers typing does not support two args
     return this.startedContainer.getMappedPort(port);
   }
 
